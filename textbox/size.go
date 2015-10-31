bc09b55c62025b28fb315011b63b958736c5edc7
He Liu <liulonnie@gmail.com>
2015-10-30 22:36:30 -0700

    move out golang related logic

--- textbox/size.go
+++ textbox/size.go
@@ -3,11 +3,9 @@ package textbox
 import (
 	"bufio"
 	"io"
-	"os"
 	"strings"
 
 	"e8vm.io/e8vm/lex8"
-	"e8vm.io/tools/goload"
 )
 
 // TabSize is the indent size for each tab
@@ -60,7 +58,8 @@ func Rect(r io.Reader) (nline, maxWidth int, e error) {
 	return nline, maxWidth, nil
 }
 
-func checkRect(log lex8.Logger, file string, r io.Reader, h, w int) {
+// CheckRect checks if a program is within a rectangular area.
+func CheckRect(log lex8.Logger, file string, r io.Reader, h, w int) {
 	br := bufio.NewReader(r)
 	row := 0
 	col := 0
@@ -95,29 +94,4 @@ func checkRect(log lex8.Logger, file string, r io.Reader, h, w int) {
 	if row > h && !strings.HasSuffix(file, "_bytes.go") {
 		log.Errorf(pos(), "too many lines")
 	}
-}
-
-// CheckRectLoaded checks all the golang source files in a loaded go program
-func CheckRectLoaded(prog *goload.Program, h, w int) []*lex8.Error {
-	errs := lex8.NewErrorList()
-
-	fset := prog.Fset
-	for _, p := range prog.Pkgs {
-		pinfo := prog.Imported[p]
-		for _, astf := range pinfo.Files {
-			tokFile := fset.File(astf.Pos())
-			name := tokFile.Name()
-			fin, e := os.Open(name)
-			if lex8.LogError(errs, e) {
-				continue
-			}
-
-			checkRect(errs, name, fin, h, w)
-			if lex8.LogError(errs, fin.Close()) {
-				continue
-			}
-		}
-	}
-
-	return errs.Errs()
 }
