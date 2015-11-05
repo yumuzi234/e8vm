4e5ae0bea9af0a691433d0fa80b16d44fd0f7476
He Liu <liulonnie@gmail.com>
2015-11-04 22:25:26 -0800

    .

--- arch8/int_bus.go
+++ arch8/int_bus.go
@@ -1,12 +1,12 @@
 package arch8
 
-// IntBus is an interrupt bus for device
+// intBus is an interrupt bus for device
 type intBus interface {
 	Ncore() byte
 	Interrupt(code byte, core byte)
 }
 
-// IntAllCores generates a interrupt to all cores on the bus.
+// intAllCores generates a interrupt to all cores on the bus.
 func intAllCores(bus intBus, code byte) {
 	ncore := bus.Ncore()
 	for i := byte(0); i < ncore; i++ {
