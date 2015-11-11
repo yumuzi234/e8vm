c31bae506061ddce7156b37d270ea7010647c004
He Liu <liulonnie@gmail.com>
2015-11-11 09:22:21 -0800

    .

--- cmd/e8asm/main.go
+++ cmd/e8asm/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io/ioutil"
 	"log"
-	"math"
+	"os"
+	"strings"
 
 	"e8vm.io/e8vm/arch8"
+	"e8vm.io/e8vm/asm8"
 	"e8vm.io/e8vm/dasm8"
+	"e8vm.io/e8vm/lex8"
 )
 
 var (
@@ -16,8 +18,6 @@ var (
 	ncycle      = flag.Int("n", 100000, "max cycles to execute")
 	memSize     = flag.Int("m", 0, "memory size; 0 for full 4GB")
 	printStatus = flag.Bool("s", false, "print status after execution")
-	bootArg     = flag.Uint("arg", 0, "boot argument, a uint32 number")
-	romRoot     = flag.String("rom", "", "rom root path")
 	randSeed    = flag.Int64("seed", 0, "random seed, 0 for using the time")
 )
 
@@ -28,16 +28,6 @@ func run(bs []byte) (int, error) {
 		return 0, err
 	}
 
-	if *bootArg > math.MaxUint32 {
-		log.Fatalf("boot arg(%d) is too large", *bootArg)
-	}
-	if err := m.WriteWord(arch8.AddrBootArg, uint32(*bootArg)); err != nil {
-		return 0, err
-	}
-
-	if *romRoot != "" {
-		m.MountROM(*romRoot)
-	}
 	if *randSeed != 0 {
 		m.RandSeed(*randSeed)
 	}
@@ -62,9 +52,25 @@ func main() {
 	}
 
 	fname := args[0]
-	bs, e := ioutil.ReadFile(fname)
+	var bs []byte
+	f, e := os.Open(fname)
 	if e != nil {
-		log.Fatal(e)
+		log.Fatalf("open: %s", e)
+	}
+
+	var es []*lex8.Error
+	if strings.HasSuffix(fname, "_bare.s") {
+		bs, es = asm8.BuildBareFunc(fname, f)
+	} else {
+		bs, es = asm8.BuildSingleFile(fname, f)
+	}
+
+	if len(es) > 0 {
+		for _, e := range es {
+			fmt.Println(e)
+		}
+		os.Exit(-1)
+		return
 	}
 
 	if *doDasm {
