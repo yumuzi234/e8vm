aa08c75133085a0bf8118b1a72f1b7df92841aae
He Liu <liulonnie@gmail.com>
2015-11-25 08:05:00 -0800

    add gfmt testing templates

---
+++ g8/gfmt/file_test.go
@@ -0,0 +1,39 @@
+package gfmt
+
+import (
+	"bytes"
+	"strings"
+	"testing"
+
+	"e8vm.io/e8vm/g8/parse"
+)
+
+func TestFormatFile(t *testing.T) {
+	gfmt := func(s string) string {
+		r := strings.NewReader(s)
+		ast, es := parse.File("", r, false)
+		if len(es) > 0 {
+			t.Errorf("parsing %q failed with errors", s)
+			for _, err := range es {
+				t.Log(err)
+			}
+		}
+
+		out := new(bytes.Buffer)
+		FprintFile(out, ast)
+		return out.String()
+	}
+
+	o := func(s, exp string) {
+		got := gfmt(s)
+		if exp != got {
+			t.Errorf("gfmt %q: expect %q, got %q", s, exp, got)
+		}
+	}
+
+	o("func main() {}", "func main() {}\n")         // tailing end line
+	o("func main  () {  }", "func main() {}\n")     // remove spaces
+	o("\n\nfunc main  () {  }", "func main() {}\n") // remove lines
+	o("func main(){}", "func main() {}\n")          // add spaces
+	o("func main() {\n}", "func main() {}\n")       // merge oneliner
+}
