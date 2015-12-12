7791a681d37db9289b92458b584c5a3ce0a6c7f2
Lonnie Liu <liulonnie@gmail.com>
2015-12-12 08:57:38 -0800

    .

--- g8/multi_test.go
+++ g8/multi_test.go
@@ -76,8 +76,28 @@ func TestMultiFile(t *testing.T) {
 			t.Errorf("got: %s", got)
 		}
 	}
+	type files map[string]string
 
-	o(map[string]string{
+	o(files{
 		"main/main.g": "func main() { }",
 	}, "")
+
+	o(files{
+		"a/a.g": "func P() { printInt(33) }",
+		"main/main.g": `
+			import ( "a" )
+			func main() { a.P() }`,
+	}, "33")
+
+	o(files{
+		"a/a.g": "struct A { func P() { printInt(33) } }",
+		"b/b.g": `import ("a"); var A a.A`,
+		"main/main.g": `import ("b"); func main() { b.A.P() }`,
+	}, "33")
+
+	o(files{
+		"a/a.g": "func init() { printInt(33) }",
+		"b/b.g": `import (_ "a"); func init() { printInt(44) }`,
+		"main/main.g": `import (_ "b"); func main() { printInt(55) }`,
+	}, "33\n44\n55")
 }
