16e517acba3a30e9110c01be27c033cdfed12fb4
He Liu <liulonnie@gmail.com>
2015-12-21 11:16:11 -0800

    .

--- g8/tast/expr.go
+++ g8/tast/expr.go
@@ -1,6 +1,8 @@
 package tast
 
 import (
+	"fmt"
+
 	"e8vm.io/e8vm/g8/types"
 	"e8vm.io/e8vm/lex8"
 )
@@ -66,9 +68,36 @@ type Para struct {
 type FuncTypeExpr struct {
 	Args []*Para
 	Rets []*Para
+	T    types.T
 }
 
 // ExprList is a list of expressions
 type ExprList struct {
 	Exprs []Expr
+}
+
+// ExprType returns the type of an expression node.
+func ExprType(expr Expr) types.T {
+	switch expr := expr.(type) {
+	case *Operand:
+		return expr.T
+	case *MemberExpr:
+		return expr.T
+	case *OpExpr:
+		return expr.T
+	case *StarExpr:
+		return expr.T
+	case *CallExpr:
+		return expr.T
+	case *IndexExpr:
+		return expr.T
+	case *ArrayTypeExpr:
+		return expr.T
+	case *Para:
+		return expr.T
+	case *FuncTypeExpr:
+		return expr.T
+	default:
+		panic(fmt.Errorf("invalid tast expr node: %T", expr))
+	}
 }
