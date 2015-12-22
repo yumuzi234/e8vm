3566868bbca81f1d575e09708946cc9279e702c6
He Liu <liulonnie@gmail.com>
2015-12-22 14:35:08 -0800

    .

--- g8/ast/decl.go
+++ g8/ast/decl.go
@@ -10,7 +10,8 @@ type IdentList struct {
 	Commas []*lex8.Token
 }
 
-// VarDecl declares a set of variable
+// VarDecl declares a set of variables. It is both a top level declaration
+// and a statement.
 type VarDecl struct {
 	Idents *IdentList
 	Type   Expr
@@ -19,7 +20,17 @@ type VarDecl struct {
 	Semi   *lex8.Token
 }
 
-// ConstDecl declares a set of constants
+// VarDecls is a variable declaration with a leading keyword
+// It could be a single decl or a decl block.
+type VarDecls struct {
+	Kw     *lex8.Token
+	Lparen *lex8.Token // optional
+	Decls  []*VarDecl
+	Rparen *lex8.Token // optional
+	Semi   *lex8.Token
+}
+
+// ConstDecl declares a set of constants.
 type ConstDecl struct {
 	Idents *IdentList
 	Type   Expr
@@ -34,16 +45,6 @@ type ConstDecls struct {
 	Kw     *lex8.Token
 	Lparen *lex8.Token // optional
 	Decls  []*ConstDecl
-	Rparen *lex8.Token // optional
-	Semi   *lex8.Token
-}
-
-// VarDecls is a variable declaration with a leading keyword
-// It could be a single decl or a decl block.
-type VarDecls struct {
-	Kw     *lex8.Token
-	Lparen *lex8.Token // optional
-	Decls  []*VarDecl
 	Rparen *lex8.Token // optional
 	Semi   *lex8.Token
 }
---
+++ g8/tast/decl.go
@@ -0,0 +1,29 @@
+package tast
+
+import (
+	"e8vm.io/e8vm/g8/types"
+)
+
+// VarDecl declares a set of variables.
+type VarDecl struct {
+	Idents []*Ref
+	Type   types.T
+	Exprs  *ExprList
+}
+
+// VarDecls is a variable declaration group.
+type VarDecls struct {
+	Decls []*VarDecl
+}
+
+// ConstDecl declares a set of constants.
+type ConstDecl struct {
+	Idents []*Ref
+	Type   types.T
+	Exprs  *ExprList
+}
+
+// ConstDecls is a const declaration group.
+type ConstDecls struct {
+	Decls []*ConstDecl
+}
