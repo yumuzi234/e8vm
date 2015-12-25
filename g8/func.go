05fb357ba0fcee9387f85d86128ed51b8005c4da
Lonnie Liu <liulonnie@gmail.com>
2015-12-24 21:13:06 -0800

    .

--- g8/func.go
+++ g8/func.go
@@ -30,6 +30,22 @@ func declareFuncSym(b *builder, f *ast.Func, t types.T) *objFunc {
 	return ret
 }
 
+func buildPkgRef(b *builder, ident *lex8.Token) *types.Pkg {
+	s := b.scope.Query(ident.Lit)
+	if s == nil {
+		b.Errorf(ident.Pos, "undefined identifier %s", ident.Lit)
+		return nil
+	}
+
+	b.spass.RefSym(s, ident.Pos)
+	if s.Type != tast.SymImport {
+		b.Errorf(ident.Pos, "%s is not an imported package", ident.Lit)
+		return nil
+	}
+
+	return s.Obj.(*objImport).ref.Type().(*types.Pkg)
+}
+
 func declareFuncAlias(b *builder, f *ast.Func, t *types.Func) *objFunc {
 	alias := f.Alias
 
