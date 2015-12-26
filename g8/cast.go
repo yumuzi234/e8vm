35ce679eaa2172368055fb5f0b638233bcd8f96e
Lonnie Liu <liulonnie@gmail.com>
2015-12-26 11:35:36 -0800

    .

--- g8/cast.go
+++ g8/cast.go
@@ -2,7 +2,6 @@ package g8
 
 import (
 	"e8vm.io/e8vm/arch8"
-	"e8vm.io/e8vm/g8/ast"
 	"e8vm.io/e8vm/g8/ir"
 	"e8vm.io/e8vm/g8/types"
 )
@@ -22,41 +21,6 @@ func regSizeCastable(to, from types.T) bool {
 		return true
 	}
 	return false
-}
-
-func buildCast(b *builder, expr *ast.CallExpr, t types.T) *ref {
-	pos := expr.Lparen.Pos
-
-	args := buildExprList(b, expr.Args)
-	if args == nil {
-		return nil
-	}
-
-	if !args.IsSingle() {
-		b.Errorf(pos, "cannot convert %s to %s", args, t)
-		return nil
-	}
-
-	srcType := args.Type()
-	ret := b.newTemp(t)
-	if c, ok := srcType.(*types.Const); ok {
-		if v, ok := types.NumConst(srcType); ok && types.IsInteger(t) {
-			return constCast(b, pos, v, t)
-		}
-		srcType = c.Type // using the underlying type
-	}
-
-	if types.IsInteger(t) && types.IsInteger(srcType) {
-		b.b.Arith(ret.IR(), nil, "cast", args.IR())
-		return ret
-	}
-	if regSizeCastable(t, srcType) {
-		b.b.Arith(ret.IR(), nil, "", args.IR())
-		return ret
-	}
-
-	b.Errorf(pos, "cannot convert from %s to %s", srcType, t)
-	return nil
 }
 
 func genCast(b *builder, from *ref, t types.T) *ref {
