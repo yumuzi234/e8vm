69da936d1b801e2402a8b52e2d2924f19dd41cb6
Lonnie Liu <liulonnie@gmail.com>
2015-12-27 14:08:11 -0800

    block

--- g8/block.go
+++ g8/block.go
@@ -1,16 +1,8 @@
 package g8
 
 import (
-	"e8vm.io/e8vm/g8/ast"
 	"e8vm.io/e8vm/g8/tast"
 )
-
-func buildBlock(b *builder, stmt *ast.Block) {
-	b.scope.Push()
-	defer b.scope.Pop()
-
-	b.buildStmts(stmt.Stmts)
-}
 
 func genBlock(b *builder, block *tast.Block) {
 	for _, s := range block.Stmts {
