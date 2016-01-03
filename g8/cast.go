cccc58e9bfda11d8406dac9287cabd101dfe497f
He Liu <liulonnie@gmail.com>
2016-01-02 23:05:39 -0800

    .

--- g8/cast.go
+++ g8/cast.go
@@ -23,6 +23,21 @@ func regSizeCastable(to, from types.T) bool {
 	return false
 }
 
+func constNumIr(v int64, t types.T) ir.Ref {
+	b, ok := t.(types.Basic)
+	if ok {
+		switch b {
+		case types.Int:
+			return ir.Snum(int32(v))
+		case types.Uint:
+			return ir.Num(uint32(v))
+		case types.Int8, types.Uint8:
+			return ir.Byt(uint8(v))
+		}
+	}
+	panic("expect an integer type")
+}
+
 func buildCast(b *builder, from *ref, t types.T) *ref {
 	srcType := from.Type()
 	ret := b.newTemp(t)
