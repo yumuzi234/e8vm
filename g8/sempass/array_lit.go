feb6c2f4efedeca00c9dc632a51ad629bb27e7e8
He Liu <liulonnie@gmail.com>
2016-03-01 15:58:35 -0800

    fix bug on empty array lit

--- g8/sempass/array_lit.go
+++ g8/sempass/array_lit.go
@@ -31,32 +31,38 @@ func buildArrayLit(b *builder, lit *ast.ArrayLiteral) tast.Expr {
 	}
 	bt := t.(types.Basic)
 
-	for _, expr := range lit.Exprs.Exprs {
-		n := b.buildConstExpr(expr)
-		if n == nil {
-			return nil
-		}
-		ntype := n.R().T
-		if _, ok := ntype.(*types.Const); !ok {
-			b.Errorf(ast.ExprPos(expr), "array literal not a constant")
-			return nil
-		}
-
-		if v, ok := types.NumConst(ntype); ok {
-			if !types.InRange(v, t) {
-				b.Errorf(ast.ExprPos(expr), "constant out of range of %s", t)
+	if lit.Exprs != nil {
+		for _, expr := range lit.Exprs.Exprs {
+			n := b.buildConstExpr(expr)
+			if n == nil {
+				return nil
+			}
+			ntype := n.R().T
+			if _, ok := ntype.(*types.Const); !ok {
+				b.Errorf(ast.ExprPos(expr), "array literal not a constant")
 				return nil
 			}
 
-			switch bt {
-			case types.Int, types.Uint:
-				var bs [4]byte
-				binary.LittleEndian.PutUint32(bs[:], uint32(v))
-				buf.Write(bs[:])
-			case types.Int8, types.Uint8:
-				buf.Write([]byte{byte(v)})
-			default:
-				panic("not integer")
+			if v, ok := types.NumConst(ntype); ok {
+				if !types.InRange(v, t) {
+					b.Errorf(
+						ast.ExprPos(expr),
+						"constant out of range of %s",
+						t,
+					)
+					return nil
+				}
+
+				switch bt {
+				case types.Int, types.Uint:
+					var bs [4]byte
+					binary.LittleEndian.PutUint32(bs[:], uint32(v))
+					buf.Write(bs[:])
+				case types.Int8, types.Uint8:
+					buf.Write([]byte{byte(v)})
+				default:
+					panic("not integer")
+				}
 			}
 		}
 	}
