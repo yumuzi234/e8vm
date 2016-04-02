c4091eb964612702d07046b92ca8c9fb038b3d87
Lonnie Liu <liulonnie@gmail.com>
2016-04-02 16:02:42 -0700

    gfmt

--- g8/gfmt/comment.go
+++ g8/gfmt/comment.go
@@ -1,42 +1,10 @@
 package gfmt
 
 import (
-	"fmt"
 	"strings"
+
+	"e8vm.io/e8vm/fmt8"
 )
-
-func indentWithSpace(s string) string {
-	ret := ""
-	for i, c := range s {
-		if c == ' ' {
-			ret += " "
-		} else if c == '\t' {
-			ret += "    "
-		} else {
-			return ret + s[i:]
-		}
-	}
-
-	return ret
-}
-
-func allSpaces(s string) bool {
-	for _, r := range s {
-		if r != ' ' {
-			return false
-		}
-	}
-	return true
-}
-
-func indentCount(s string) int {
-	for i, r := range s {
-		if r != ' ' {
-			return i
-		}
-	}
-	return len(s)
-}
 
 func formatComment(c string) string {
 	if strings.HasPrefix(c, "//") {
@@ -48,54 +16,8 @@ func formatComment(c string) string {
 	}
 
 	// block comment here
-	fmt.Println(c)
 	body := strings.TrimPrefix(c, "/*")
 	body = strings.TrimSuffix(body, "*/")
-
-	lines := strings.Split(body, "\n")
-	nline := len(lines)
-
-	// convert indent into space, make blank lines empty.
-	for i := range lines {
-		if i == 0 {
-			continue
-		}
-		line := lines[i]
-		line = indentWithSpace(line)
-		if allSpaces(line) {
-			line = ""
-		}
-		lines[i] = line
-	}
-
-	if nline > 1 {
-		minIndentCount := indentCount(lines[1])
-		for i, line := range lines {
-			if i <= 1 {
-				continue
-			}
-			if line == "" {
-				continue
-			}
-
-			n := indentCount(line)
-			if n < minIndentCount {
-				minIndentCount = n
-			}
-		}
-
-		for i := range lines {
-			if i == 0 {
-				continue
-			}
-			if lines[i] == "" {
-				continue
-			}
-			// trim prefix of minIndent
-			lines[i] = lines[i][minIndentCount:]
-		}
-	}
-
-	ret := strings.Join(lines, "\n")
-	return "/*" + ret + "*/"
+	body = fmt8.BoxSpaceIndent(body)
+	return "/*" + body + "*/"
 }
