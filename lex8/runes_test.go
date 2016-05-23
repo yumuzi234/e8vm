5ceb0d04cc3ece8036079adb935771dd375341d4
yumuzi <yumuzi234@gmail.com>
2016-05-23 11:47:36 -0700

    runes_test.go added

--- lex8/runes_test.go
+++ lex8/runes_test.go
@@ -1,3 +1,4 @@
+
 package lex8
 
 import (
@@ -5,6 +6,7 @@ import (
 )
 
 func TestIsLetter(t *testing.T) {
+	t.Log ("Testing IsLetter")
 	for _, r := range "abzdATZ" {
 		if !IsLetter(r) {
 			t.Errorf("%v should be a letter", r)
@@ -17,3 +19,41 @@ func TestIsLetter(t *testing.T) {
 		}
 	}
 }
+
+func TestIsDigit(t *testing.T) {
+	t.Log ("Testing IsDigit")
+        for _, r := range "0123456789" {
+                if !IsDigit(r) {
+                        t.Errorf("%v should be a digit", r)
+                }
+        }
+
+	for _, r := range "abzATZ#%~" {
+		if IsDigit(r) {
+                        t.Errorf("%v should not be a digit", r)
+                }
+	}
+}
+
+
+func TestIsHexDigit(t *testing.T) {
+        t.Log ("Testing IsHexDigit")
+        for _, r := range "0123456789" {
+                if !IsHexDigit(r) {
+                        t.Errorf("%v should be a hexdigit", r)
+                }
+        }
+
+        for _, r := range "abcdefABCDEF"{
+                if !IsHexDigit(r) {
+                        t.Errorf("%v should be a hexdigit", r)
+                }
+        }
+
+        for _, r := range "gJmXY!@*"{
+                if IsHexDigit(r) {
+                        t.Errorf("%v should not be a hexdigit", r)
+                }
+        }
+}
+
