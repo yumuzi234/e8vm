3500a4b2f7ff67f71deb6231ab2247f32e066ded
yumuzi <yumuzi234@gmail.com>
2016-05-23 16:19:46 -0700

    after gofmt, LF/CRLF

--- lex8/runes_test.go
+++ lex8/runes_test.go
@@ -1,4 +1,3 @@
-
 package lex8
 
 import (
@@ -6,7 +5,7 @@ import (
 )
 
 func TestIsLetter(t *testing.T) {
-	t.Log ("Testing IsLetter")
+	t.Log("Testing IsLetter")
 	for _, r := range "abzdATZ" {
 		if !IsLetter(r) {
 			t.Errorf("%v should be a letter", r)
@@ -21,39 +20,37 @@ func TestIsLetter(t *testing.T) {
 }
 
 func TestIsDigit(t *testing.T) {
-	t.Log ("Testing IsDigit")
-        for _, r := range "0123456789" {
-                if !IsDigit(r) {
-                        t.Errorf("%v should be a digit", r)
-                }
-        }
+	t.Log("Testing IsDigit")
+	for _, r := range "0123456789" {
+		if !IsDigit(r) {
+			t.Errorf("%v should be a digit", r)
+		}
+	}
 
 	for _, r := range "abzATZ#%~" {
 		if IsDigit(r) {
-                        t.Errorf("%v should not be a digit", r)
-                }
+			t.Errorf("%v should not be a digit", r)
+		}
 	}
 }
 
-
 func TestIsHexDigit(t *testing.T) {
-        t.Log ("Testing IsHexDigit")
-        for _, r := range "0123456789" {
-                if !IsHexDigit(r) {
-                        t.Errorf("%v should be a hexdigit", r)
-                }
-        }
+	t.Log("Testing IsHexDigit")
+	for _, r := range "0123456789" {
+		if !IsHexDigit(r) {
+			t.Errorf("%v should be a hexdigit", r)
+		}
+	}
 
-        for _, r := range "abcdefABCDEF"{
-                if !IsHexDigit(r) {
-                        t.Errorf("%v should be a hexdigit", r)
-                }
-        }
+	for _, r := range "abcdefABCDEF" {
+		if !IsHexDigit(r) {
+			t.Errorf("%v should be a hexdigit", r)
+		}
+	}
 
-        for _, r := range "gJmXY!@*"{
-                if IsHexDigit(r) {
-                        t.Errorf("%v should not be a hexdigit", r)
-                }
-        }
+	for _, r := range "gJmXY!@*" {
+		if IsHexDigit(r) {
+			t.Errorf("%v should not be a hexdigit", r)
+		}
+	}
 }
-
