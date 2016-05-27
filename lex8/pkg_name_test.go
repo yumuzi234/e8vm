332e234ca916caadec334a8616226c3c597fd1b8
yumuzi <yumuzi234@gmail.com>
2016-05-26 18:05:51 -0700

    add pkg name test

---
+++ lex8/pkg_name_test.go
@@ -0,0 +1,20 @@
+package lex8
+
+import (
+	"testing"
+)
+
+func TestIsPkgName(t *testing.T) {
+	testName := []string{"a", "abc", "a12", "a12b"}
+	for _, name := range testName {
+		if !IsPkgName(name) {
+			t.Errorf("%v should be a package name", name)
+		}
+	}
+	testName = []string{"aB", "1abc", "", "  ", "a~", "%a", "A1", "$abc1"}
+	for _, name := range testName {
+		if IsPkgName(name) {
+			t.Errorf("%v should not be a package name", name)
+		}
+	}
+}
