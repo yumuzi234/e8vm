8e3621f35f54bc05b7828891922f03aeb27ce2db
Lonnie Liu <liulonnie@gmail.com>
2016-06-17 00:53:48 -0700

    .

---
+++ lex8/words.go
@@ -0,0 +1,38 @@
+package lex8
+
+import (
+	"io"
+)
+
+// Token types for the example lexer.
+const (
+	Word = iota
+	Punc
+)
+
+func lexWord(x *Lexer) *Token {
+	r := x.Rune()
+	if IsLetter(r) || IsDigit(r) {
+		// it is a word
+		for {
+			r, _ := x.Next()
+			if x.Ended() || !(IsLetter(r) || IsDigit(r)) {
+				break
+			}
+		}
+		return x.MakeToken(Word)
+	}
+
+	x.Next()
+	return x.MakeToken(Punc)
+}
+
+// NewWordLexer returns an example lexer that parses a file
+// into words and punctuations.
+func NewWordLexer(file string, r io.Reader) *Lexer {
+	ret := MakeLexer(file, r, lexWord)
+	ret.IsWhite = func(r rune) bool {
+		return r == ' ' || r == '\t' || r == '\r' || r == '\n'
+	}
+	return ret
+}
