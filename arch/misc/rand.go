e6e799e688373c9dd9cb7d5882b43709ec5357a3
He Lonnie Liu <liulonnie@gmail.com>
2016-09-08 16:55:31 -0400

    rand

--- arch/misc/rand.go
+++ arch/misc/rand.go
@@ -1,5 +1,20 @@
 package misc
 
+import (
+	"io"
+)
+
 // Rand provides a random number generator.
 type Rand struct {
+	r io.Reader
+}
+
+// NewRand creates a new random number generator.
+func NewRand(seed int64) *Rand {
+	panic("todo")
+}
+
+// Handle handles incoming request to generate a random number.
+func (r *Rand) Handle(_ []byte) ([]byte, int32) {
+	panic("todo")
 }
