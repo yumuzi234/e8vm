68c3882ae3239501b4170120d8826ddbf7c807ad
Zhen Li <yumuzi234@gmail.com>
2017-01-10 11:29:41 -0800

    file_test.go

--- pl/parse/file.go
+++ pl/parse/file.go
@@ -30,7 +30,7 @@ func parseTopDecl(p *parser) ast.Decl {
 
 	if len(p.Errs()) == 0 {
 		// we only complain about this when there is no other error
-		p.ErrorfHere("expect top level declaration")
+		p.CodeErrorfHere("pl.expectTopDecl", "expect top level declaration")
 	} else {
 		p.Jail()
 	}
