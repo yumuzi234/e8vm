88e4cfde852039423901e7a30cf205012cdc5af1
He Liu <liulonnie@gmail.com>
2017-02-09 23:13:17 -0800

    .

--- pl/codegen/offset.go
+++ pl/codegen/offset.go
@@ -1,17 +1,9 @@
 package codegen
 
 func addOffsetHigh(b *Block, reg uint32, offset int32) {
-	if offset >= 0 {
-		hi := uint32(offset) >> 16
-		if hi > 0 {
-			b.inst(asm.addui(reg, reg, hi))
-		}
-		return
-	}
-
-	// addi will add a 0xffff on the high 16-bit
-	// we need to add 1 extra to cancel that out.
-	hi := ((uint32(offset) >> 16) + 1) & 0xffff
+	u := (uint32)(offset)
+	sign := (u >> 15) & 0x1
+	hi := ((u >> 16) + sign) & 0xffff
 	if hi > 0 {
 		b.inst(asm.addui(reg, reg, hi))
 	}
