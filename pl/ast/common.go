c5e318342085de2c79c9c91b4b13f7453c9ff15d
Zhen Li <yumuzi234@gmail.com>
2017-03-23 20:15:02 -0700

    .

---
+++ pl/ast/common.go
@@ -0,0 +1,11 @@
+package ast
+
+// Expr is a general expression in the language
+type Expr interface{}
+
+// Stmt is a general statement
+type Stmt interface{}
+
+// Decl is a general declaration,
+// could be var, function, struct or interface
+type Decl interface{}
